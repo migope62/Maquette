d852bb04dbce799afcc3dd64e1b1c374498b08ad
Maxime Vande <Maxime62580@laposte.net>
2022-12-21 11:27:53 +0100

    A

--- app.js
+++ app.js
@@ -1,9 +1,61 @@
-
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
 
-app.use(express.static("public"));
+app.use(express.static("public"))
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+mongoose.set('strictQuery', false)
+mongoose.connect('mongodb://localhost/pas', { useNewUrlParser: true });
+
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    mail: String,
+    message: String
+
+})
+const forms_schema = mongoose.Schema(
+    {
+
+        name: { type: String, required: true },
+        mail: { type: String, required: true }
+
+
+    }
+);
+const pas = mongoose.model('form', forms_schema);
+app.get('/pas', (req, res) => {
+    forms.find({}, (err, pasa) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(pasa);
+        }
+    })
+});
+app.post('/pas', (req, res) => {  /*Recevoir*/
+
+    const Object = new pas({
+
+        name: req.body.user_name,
+        mail: req.body.user_mail
+
+
+    });
+    Object.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('connecté');
+        }
+    });
+});
+
 app.get("/index.ejs", (req,res) => {
     res.render("index.ejs")
 })
@@ -37,4 +89,7 @@ app.get("/page11.ejs", (req,res) => {
 app.get("/page5.ejs", (req,res) => {
     res.render("page5.ejs")
 })
-app.listen(8080);
+
+app.listen(3001, (req, res) => {
+    console.log("Serveur lancé sur le port 3001...")
+});
